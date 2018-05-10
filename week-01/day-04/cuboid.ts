1d557d55ef9f5cdd7c68a6f200471b79246ac947
Sorsi <orsolya.suto.so@gmail.com>
2018-05-11 00:16:42 +0200

    calculated cuboid datas with var

--- week-01/day-04/cuboid.ts
+++ week-01/day-04/cuboid.ts
@@ -2,3 +2,15 @@ export {}
 
 'use strict';
 
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
+
+let a: number = 20;
+let b: number = 30;
+let c: number = 40;
+
+console.log('Surface Area is ' + 2*(a*b + a*c + b*c));
+console.log('Volume is ' + a*b*c);
