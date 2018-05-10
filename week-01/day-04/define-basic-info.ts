cee91edf098d837cf2b555b6028cc5ebca246026
Sorsi <orsolya.suto.so@gmail.com>
2018-05-10 16:02:17 +0200

    wrote basic infos about myself

---
+++ week-01/day-04/define-basic-info.ts
@@ -0,0 +1,20 @@
+export {}
+
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let myName: string = 'Orsi'
+let myAge: number = 29;
+let myHeight: number = 1.68;
+let myStatus: boolean = false;
+
+console.log(`My name is ${myName}
+My age is ${myAge}
+My height is ${myHeight} m
+Married: ${myStatus}`);
+
