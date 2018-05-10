ba1bba1971cae4304956bd44bc03b2a2bbae7dca
Sorsi <orsolya.suto.so@gmail.com>
2018-05-10 11:53:11 +0200

    changed order of Humpty Dumpty story with var

---
+++ week-01/day-04/humpty-dumpty.ts
@@ -0,0 +1,6 @@
+var a = 'All the king\'s horses and all the king\'s men';
+var b = 'Humpty Dumpty had a great fall.';
+var c = 'Humpty Dumpty sat on a wall,';
+var d = 'Couldn\'t put Humpty together again.';
+
+console.log(c, b, a, d);
