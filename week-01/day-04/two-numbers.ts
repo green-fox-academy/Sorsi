622b4cf6f25066016b0f8c9d1d137221d6634232
Sorsi <orsolya.suto.so@gmail.com>
2018-05-10 14:56:00 +0200

    number operations

--- week-01/day-04/two-numbers.ts
+++ week-01/day-04/two-numbers.ts
@@ -1,6 +1,11 @@
+export{}
+
 'use strict';
 
 let myVar1: number = 22;
 let myVar2: number = 13;
 
-console.log(myVar1 + myVar2)
+console.log(myVar1 + myVar2);
+console.log(myVar1 - myVar2);
+console.log(myVar1 * myVar2);
+console.log(myVar1 / myVar2);
