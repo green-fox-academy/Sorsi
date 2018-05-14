88483c8c16a9836eaf8336d9b40ffaaac6373d3d
Sorsi <orsolya.suto.so@gmail.com>
2018-05-14 14:35:41 +0200

    added param

---
+++ week-02/day-01/print-params.ts
@@ -0,0 +1,13 @@
+export{}
+
+'use strict';
+
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+function printParams(name: string, age: number): void {
+    console.log(name, age)
+}
+
+printParams('Peter', 234)
