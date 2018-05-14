63ea8fedd7a599d60febbe44741a35991db578ed
Sorsi <orsolya.suto.so@gmail.com>
2018-05-14 16:43:39 +0200

    add + 'a' to words with .map

---
+++ week-02/day-01/append-a.ts
@@ -0,0 +1,14 @@
+export{}
+
+'use strict';
+
+// -  Create an array variable named `animals`
+//    with the following content: `["kuty", "macsk", "cic"]`
+// -  Add all elements an `"a"` at the end
+
+let animals: string [] = ['kuty', 'macsk', 'cic']
+animals = animals.map((x, index) => {
+    return x += 'a';
+})
+
+console.log(animals)
