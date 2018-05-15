64570d7e56e571bde74a510343c5fefba53d3818
Sorsi <orsolya.suto.so@gmail.com>
2018-05-15 10:50:00 +0200

    replaced two words

---
+++ week-02/day-02/string.ts
@@ -0,0 +1,90 @@
+
+export{}
+
+'use strict';
+
+let str: string = 'This is my original string';
+
+/*
+console.log(str.indexOf('original'));
+console.log(str.indexOf('!'));
+console.log(str.indexOf('is', 4)); 
+
+console.log(str.replace('original', 'new'));
+console.log(str);
+
+str = str.replace('original', 'new');
+console.log(str);
+
+let appleStr: string = 'apple';
+console.log(appleStr.split(''));
+console.log(str.split(''));
+console.log(str.split('', 3));
+
+
+console.log(str.slice(8, 19));
+console.log(str.slice(8, -3)); // hátulról számolja a sorrendet
+console.log(str);
+
+console.log(str.substring(8, 19)); // hasonló a slicehoz, de itt ha az első paraméter nagyobb , mint a második, akkor ki lehet vmit cserélni
+
+
+console.log(str.charAt(11));
+console.log(str[11]);
+
+console.log(str.concat(' I like it.')); // rosszul néz ki, nem hasznos
+console.log(`${str} I like it.`); // ezt használd!
+
+body {
+    color: red,
+}
+
+
+let car = {
+    brand: 'Audi',
+    type: 'A6',
+    color: 'blue',
+    start: () => {
+        console.log('Wrom-wrom');
+    }
+};
+
+console.log(typeof car);
+console.log(car);
+console.log(car.type);
+
+car.type = 'A1';
+car['type'] = 'TT';
+console.log(car.type);
+
+let keyStr = 'color';
+
+console.log(car.type);
+console.log(car[keyStr]);
+
+console.log(Object.keys(car));
+
+IDBObjectStore.keys(car).forEach( => {
+    console.log(`${car[key]}`);
+});
+
+console.log(car.start());
+
+console.log(car.hasOwnProperty('color'));
+
+let objArray: any [] = [];
+objArray.push(car);
+objArray.push({
+    brand: 'Audi',
+    type: 'A6',
+    color: 'blue',
+    start: () => {
+        console.log('Wrom-wrom');
+});
+
+console.log(objArray);
+objArray.forEach(car => {
+    console.log(`I have a(n) ${car.brand} - ${car.color}`);
+});
+
+*/
