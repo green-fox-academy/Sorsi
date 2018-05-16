a77b894ca00438465b505c617b690292fa672226
Sorsi <orsolya.suto.so@gmail.com>
2018-05-16 16:18:45 +0200

    wrote multiplied lines juhu

---
+++ week-02/day-03/write-multiple-lines.ts
@@ -0,0 +1,32 @@
+export { }
+'use strict';
+
+// Create a function that takes 3 parameters: a path, a word and a number,
+// than it should write to a file.
+// The path parameter should be a string, that describes the location of the file.
+// The word parameter should be a string, that will be written to the file as lines
+// The number paramter should describe how many lines the file should have.
+// So if the word is 'apple' and the number is 5, than it should write 5 lines
+// to the file and each line should be 'apple'
+// The function should not raise any error if it could not write the file.
+
+declare function require(path: string): any;
+let fs = require('fs');
+
+function addApple(filePath: string, content: string, lineNum: number) {
+  try {
+    let apple: string = '';
+    for (let i: number = 0; i < lineNum; i++) {
+      apple += `${content}\r\n`;
+    }
+    console.log(apple);
+    fs.writeFileSync(filePath, apple);
+    console.log('juhu');
+  } catch (error) {
+    console.log(error.message);
+    console.log('Fail');
+  }
+  return null;
+}
+
+addApple('multilines.txt', 'apple', 5);
