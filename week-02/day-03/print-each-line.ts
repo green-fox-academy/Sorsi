895df2bccf7f55583d87945feb65d2d422b3ca11
Sorsi <orsolya.suto.so@gmail.com>
2018-05-16 13:35:23 +0200

    printed ech lines of a text file with try,catch, return

--- week-02/day-03/print-each-line.ts
+++ week-02/day-03/print-each-line.ts
@@ -4,6 +4,7 @@ export {}
 // Write a program that opens a file called 'my-file.txt', then prints
 // each of lines form the file.
 // If the program is unable to read the file (for example it does not exists),
+//then it should print an error message like: 'Unable to read file: my-file.txt'
 
 declare function require(path: string): any;
 let fs = require('fs');
@@ -11,17 +12,12 @@ let charEncoding = 'utf-8';
 
 function readFromFile(filePath: string) {
     try {
-        return fs.readFileSync('my-file.txt', charEncoding);
+        return fs.readFileSync('my-fie.txt', charEncoding);
     } catch(error) {
-        console.log(('it does not exists'));
+        console.log(('Unable to read file: my-file.txt'));
         return null;
         //result: string = readFromFile('my-file.txt');
     }
 }
 
-let result = readFromFile('my-file.txt');
-if (result !== null) {
-    result.split('\r\n').forEach(line => {
-        console.log(line.split('').length);
-    });
-}
+console.log(readFromFile('my-file.txt'))
