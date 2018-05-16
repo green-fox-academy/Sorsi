2780fd46304f8cd5c877d752b92b7108590c8dfa
Sorsi <orsolya.suto.so@gmail.com>
2018-05-16 11:38:22 +0200

    divided by zero if statment

---
+++ week-02/day-03/divide-by-zero.ts
@@ -0,0 +1,20 @@
+export {}
+'use strict';
+
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print 'fail' if the parameter is 0
+
+let myNumber: number = 0;
+
+function division(a: number): any {
+  let num = myNumber / 10;
+    if (myNumber === 0) {
+        console.log('Fail!')
+    } else {
+    return num
+    }   
+}
+
+console.log(division(myNumber))
