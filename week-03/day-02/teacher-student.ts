2cf3501564d345be7d475fb34eca379a70c3ddfb
Sorsi <orsolya.suto.so@gmail.com>
2018-05-23 19:04:19 +0200

    questions and answers

---
+++ week-03/day-02/teacher-student.ts
@@ -0,0 +1,25 @@
+'use strict'
+
+/*
+Create Student and Teacher classes
+Student
+learn()
+question(teacher) -> calls the teachers answer method
+Teacher
+teach(student) -> calls the students learn method
+answer()
+*/
+
+class Student {
+  learn() {
+  }
+    question(teacher) {
+      teacher.answer();
+    }  
+}
+
+class Teacher {
+  teach(student) {
+    student.learn();
+  }
+}
