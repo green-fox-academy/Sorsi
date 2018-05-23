f0efc153074ee5613efffa5d5ec18f2cb1f7cad3
Sorsi <orsolya.suto.so@gmail.com>
2018-05-23 23:58:02 +0200

    factorial with recursion

---
+++ week-03/day-04/counter.ts
@@ -0,0 +1,14 @@
+'use strict'
+
+// Write a recursive function that takes one parameter: n and counts down from n.
+
+function counter(n: number): number {
+  if (n > 0) {
+    console.log(n);
+    return counter(n - 1);
+  } else {
+    return n;
+  }
+}
+
+counter(10);
