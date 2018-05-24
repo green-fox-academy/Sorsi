118c7341098199311717729dd223859bc56c11d2
Sorsi <orsolya.suto.so@gmail.com>
2018-05-24 10:56:53 +0200

    non-negative numbers digits sum with recursion

---
+++ week-03/day-04/sumdigit.ts
@@ -0,0 +1,16 @@
+'use strict'
+
+// Given a non-negative int n, return the sum of its digits recursively (no loops). 
+// Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while 
+// divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+function SumDigits (n: number) {
+  console.log(n)
+  if (n > 9) {
+      return n % 10 + SumDigits(Math.floor(n / 10));
+  } else {
+    return n;
+  }
+}
+
+console.log(SumDigits(56479));
