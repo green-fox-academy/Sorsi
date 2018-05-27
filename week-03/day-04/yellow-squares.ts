a5b11d6dac0ed703342e242d749ce607f1bdbcc4
Sorsi <orsolya.suto.so@gmail.com>
2018-05-27 19:01:17 +0200

    yellow squares with recursion

--- week-03/day-04/yellow-squares.ts
+++ week-03/day-04/yellow-squares.ts
@@ -3,9 +3,18 @@
 const canvas = document.querySelector('.canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-let size: number = 100;
-
-function drawSqare() {
-ctx.fillStyle = 'black';
-    ctx.strokeRect(100, 0, size, size)
+function drawSquare(x: number, y: number, size: number, i: number) { 
+  ctx.strokeRect(x, y, size, size);
+  if (i <= 1) {
+    return 1;
+  } 
+  else {    
+    drawSquare(x + size / 3, y, size / 3, i - 1);
+    drawSquare(x, y + size / 3, size / 3, i - 1);
+    drawSquare(x + size / 3 * 2, y + size / 3, size / 3, i - 1);
+    drawSquare(x + size / 3, y + size / 3 * 2, size / 3, i - 1);
+    return;
   }
+}
+
+drawSquare(0, 0, canvas.width, 6)
