a862ae8e917c01ae23613f485f88e9814f513c10
Sorsi <orsolya.suto.so@gmail.com>
2018-05-31 17:30:35 +0200

    letter testing

---
+++ week-04/day-04/letter-count.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+function letterCount(text:string): Object {
+  let counter: Object = {};
+  for (let i: number = 0; i < text.length; i++) {
+    let letter = text[i];
+    if (counter[letter]) {
+      counter[letter]++;
+    } else {
+      counter[letter] = 1;
+    }
+  }
+  return counter;
+}
+
+export { letterCount }
