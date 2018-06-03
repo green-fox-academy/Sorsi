a8783249eeca51e80ed9793c07e9dd268c8149b6
Sorsi <orsolya.suto.so@gmail.com>
2018-06-03 10:01:28 +0200

    used join method to conact strings with spaces in an array

--- week-02/day-02/quote-swap.ts
+++ week-02/day-02/quote-swap.ts
@@ -10,9 +10,9 @@
 let words: string[] = ['What', 'I', 'do', 'create,', 'I', 'cannot', 'not', 'understand.'];
 
 function quoteSwap(list: string[]) {
-  list.splice(2, 1, 'cannot');
+  list.splice(2, 1, list[5]);
   list.splice(5, 1, 'do');
-  return ` ${list[0]} ${list[1]} ${list[2]} ${list[3]} ${list[4]} ${list[5]} ${list[6]} ${list[7]}`;
+  return list.join(' ');
 }
 
 console.log(quoteSwap(words));
