34b73ab198d0287c336e45abcab7ef2aa3a0e459
Sorsi <orsolya.suto.so@gmail.com>
2018-06-20 11:57:35 +0200

    added query

---
+++ week-06/day-03/queryselector/queryselector-1.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let king = document.querySelector('#b325'); // # elemre . classra ; ha nem írok semmit pl. p tag
+//console.log(king);
+
+let businessLamp = document.querySelectorAll('.big');
+//console.log(businessLamp)
+
+for (let i = 0; i < businessLamp.length; i++) {
+  //console.log(businessLamp[i].textContent);
+}
+
+let conceitedKing = document.querySelectorAll('.container');
+
+for (let i = 0; i < conceitedKing.length; i++) {
+  //console.log(conceitedKing[i].textContent);
+}
+
+let noBusiness = document.querySelectorAll('div');
+console.log(noBusiness)
+
+for (let i = 0; i < noBusiness.length; i++) {
+  console.log(noBusiness[i].textContent);
+}
