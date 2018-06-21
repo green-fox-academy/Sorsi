df96bb6273f2d0abc376514bb9a88fb8c11251c1
Sorsi <orsolya.suto.so@gmail.com>
2018-06-21 16:35:01 +0200

    added todos

---
+++ week-06/day-04/todos/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const todos = [
+  'get up',
+  'survive',
+  'go back to bed',
+];
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('todos', {
+    todos,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
