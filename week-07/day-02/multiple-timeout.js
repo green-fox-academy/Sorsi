9c3f87240651908712e1206133e67e368f9c78d8
Sorsi <orsolya.suto.so@gmail.com>
2018-06-26 11:25:33 +0200

    added multiply-timeout

---
+++ week-07/day-02/multiple-timeout.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Write a program that prints the following fruits:
+// apple -> immediately
+// pear -> after 1 seconds
+// melon -> after 3 seconds
+// grapes -> after 5 seconds
+
+setTimeout(() => {
+  console.log('apple'); // prints after one second
+}, 3000);
+
