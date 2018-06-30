50ac6e07826a19b5616011ccfad2e5470a35bc30
Sorsi <orsolya.suto.so@gmail.com>
2018-06-30 09:06:18 +0200

    changed ul background

--- week-06/day-03/DOM/02-innerHTML/content-2.js
+++ week-06/day-03/DOM/02-innerHTML/content-2.js
@@ -6,6 +6,11 @@ let list = document.querySelectorAll('li')
 
 console.log(list)
 
+/* 1) */
 for (let i = 0; i < list.length; i++) {
   console.log(list[i].innerHTML = animals[i]);
 }
+
+/* 2) */
+let list2 = document.querySelector('ul');
+list2.classList.add("backGround");
