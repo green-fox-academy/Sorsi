0c948dc1c9260791cac4fef8cbae6e663d29962b
Sorsi <orsolya.suto.so@gmail.com>
2018-07-03 17:23:24 +0200

    added three-times solution

--- week-08/day-02/06-click-three-times.js
+++ week-08/day-02/06-click-three-times.js
@@ -6,5 +6,18 @@ If the user clicks the button 3 times, and 5 seconds is already elapsed since th
 If the user starts clicking before the first 5 seconds, nothing should be printed */
 
 let button = document.querySelector('button');
+let message = document.createElement('p');
+let body = document.querySelector('body');
 
-setTimeout(() => { document.querySelector('p').innerText = 'HELLO' }, 5000);
+let counter = 0;
+
+button.addEventListener('click', () => {
+  counter++;
+  console.log('nothing')
+  if (counter === 3) {
+    setTimeout(() => {
+      body.appendChild(message);
+      message.innerText = '5 seconds elapsed and clicked 3 times';
+    }, 5000);
+  }
+});
