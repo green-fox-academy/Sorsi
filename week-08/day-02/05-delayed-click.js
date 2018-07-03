043c79c9d9f4c5a7664ffeb7ae3639657c262051
Sorsi <orsolya.suto.so@gmail.com>
2018-07-03 14:56:27 +0200

    modified js

--- week-08/day-02/05-delayed-click.js
+++ week-08/day-02/05-delayed-click.js
@@ -4,8 +4,9 @@
 If the user clicks the button it should wait 2 seconds and it should show a text under the button: 2 seconds ellapsed */
 
 let button = document.querySelector('button');
-setTimeout(function(){ document.querySelector('p').innerText = 'HELLO' }, 2000);
 
-button.addEventListener('click', setTimeout)
-
-/* setTimeout(() => greet('secret'), 1000); */
+button.addEventListener('click', () => {
+  setTimeout(() => { 
+    document.querySelector('p').innerText = 'HELLO' 
+  }, 2000);
+});
