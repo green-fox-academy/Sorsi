121889e68a211991ca67690ed9e0b48d3bf6ec5f
Sorsi <orsolya.suto.so@gmail.com>
2018-07-03 13:54:55 +0200

    modified the directory and file structure

---
+++ week-08/day-02/04-bubbling.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let navElement = document.querySelectorAll('nav button');
+console.log(navElement)
+
+navElement.forEach(element => {
+  if (element.innerHTML = 'Up') {
+    
+  }
+});
