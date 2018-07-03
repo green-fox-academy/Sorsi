20fe8631c93777bc950671bd326e8e444932ae4b
Sorsi <orsolya.suto.so@gmail.com>
2018-07-03 13:44:29 +0200

    one-time-clicker solved

--- week-08/day-02/one-time-clicker.js
+++ week-08/day-02/one-time-clicker.js
@@ -2,26 +2,23 @@
 
 let button = document.querySelector('button');
 
+//-----------------FIRST VERSION-------------------------
 function getTime(event) {
   let time = document.getElementById('time');
   time.firstChild.nodeValue = event.timeStamp;
 }
 
-/* button.addEventListener('click', (e) => {
-  if(button.disabled = true) {
-    button.disabled = false;
-  } else {
-    button.disabled = false;
-  }
-}); */
-
 function disableButton() {
-  if (button.disabled) {
-    button.disabled = false;
-  } else {
-    button.disabled = true;
-  }
+  button.disabled = true;
 }
 
 button.addEventListener('click', getTime);
 button.addEventListener('click', disableButton);
+
+//-----------------SECOND VERSION-------------------------
+button.addEventListener('click', () => {
+  if (!button.disabled) {
+    console.log(Date.now());
+    button.disabled = true;
+  }
+});
