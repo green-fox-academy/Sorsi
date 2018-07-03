8c58f070bf182e23175e000c150614af176ab0e0
Sorsi <orsolya.suto.so@gmail.com>
2018-07-03 13:51:34 +0200

    solved keyup exercise

--- week-08/day-02/keyup.js
+++ week-08/day-02/keyup.js
@@ -1,8 +1,7 @@
 'use strict';
 
-
-window.addEventListener('keyup', () => {
+window.addEventListener('keyup', event => {
+  console.log(event);
   document.body.style.background = 'red';
+  document.querySelector('h1').innerText = `${event.key} is the last pressed key.`; 
 });
-
-document.querySelector('h1').innerHTML
