f6f1f2063210bc16a8984cb321f356f030547407
Sorsi <orsolya.suto.so@gmail.com>
2018-07-04 17:33:33 +0200

    added clicks

--- week-08/day-03/02-Button-Disable/button-disable.js
+++ week-08/day-03/02-Button-Disable/button-disable.js
@@ -1,16 +1,27 @@
 'use strict';
 
 window.onload = () => {
-  
-  let buttons = document.querySelectorAll('button');
-  console.log(buttons)
 
-  buttons.forEach(element => {
-    element.disabled = true;
-    element.addEventListener('click', () => {
-      alert(`Thank you, you've successfully signed up for cat facts`)
-    })
+  /* console.log(document.querySelectorAll(`input[name='fav-animal']:checked`))
+  let favAnimals = document.querySelectorAll(`input[name='fav-animal']`); */
+  let catButton = document.getElementById('cat-lover');
+  let signUpButton = document.getElementById('sign-up');
+
+  document.addEventListener('click', () => {
+    if (document.getElementById('dog').checked || document.getElementById('cat').checked) {
+      signUpButton.disabled = false;
+    } 
+    if (document.getElementById('yes').checked) {
+      catButton.disabled = false;
+    }
   });
-  
+
+  catButton.addEventListener('click', () => {
+    alert(`Thank you, you've successfully signed up for cat facts`);
+  });
+
+  signUpButton.addEventListener('click', () => {
+    alert(`Sigh, we still added you to the cat facts list`);
+  });
 
 }
