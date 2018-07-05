8f01684e1d72b4d9bcd3f0edb5ff00ea19a5878b
Sorsi <orsolya.suto.so@gmail.com>
2018-07-05 14:10:05 +0200

    almost forget: puuussssh

--- week-07/day-05/REDDIT PROJECT/public/frontend.js
+++ week-07/day-05/REDDIT PROJECT/public/frontend.js
@@ -1,16 +1,47 @@
 'use strict';
+window.onload = () => {
+    const host = 'http://localhost:3000';
 
-const http = new XMLHttpRequest();
-const host = 'http://localhost:3000';
+    const http = new XMLHttpRequest();
+    http.onload = () => {
+        const result = JSON.parse(http.responseText);
+        console.log(result.posts[0]);
 
-http.onload = () => {
-    const result = JSON.parse(http.responseText);
-    console.log(result);
+        let owner = result.posts[0].owner;
+
+        const content = document.querySelector('.content');
+
+        result.posts.forEach(post => {
+            let modify = document.createElement('button');
+            modify.innerText = 'Modify';
+            let remove = document.createElement('button');
+            remove.innerText = 'Remove';
+            let p = document.createElement('p');
+
+            let title = document.createElement('h1');
+            title.innerText = post.title;
+
+            let timestamp = document.createElement('h4');
+            timestamp.innerText = post.timestamp;
+
+            let url = document.createElement('h6');
+            url.innerText = post.url;
+
+            let score = document.createElement('h3');
+            score.innerText = post.score;
+
+            p.appendChild(title);
+            p.appendChild(timestamp);
+            p.appendChild(url);
+            p.appendChild(score);
+            p.appendChild(modify);
+            p.appendChild(remove);
+            content.appendChild(p);
+        });        
+        
+    }
+
+    http.open('GET', `${host}/api/posts`, true);
+    http.send();
+
 }
-
-const redditContent = document.querySelector('.content');
-redditConten.appendChild
-
-
-http.open('GET', `${host}/api/posts`, true);
-http.send();
