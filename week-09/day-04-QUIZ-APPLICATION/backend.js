87e42d719d4749df7d383067636249835fae291a
Sorsi <orsolya.suto.so@gmail.com>
2018-07-12 12:26:33 +0200

    modified backend

--- week-09/day-04-QUIZ-APPLICATION/backend.js
+++ week-09/day-04-QUIZ-APPLICATION/backend.js
@@ -8,12 +8,14 @@ const path = require('path');
 
 app.use(express.json());
 app.use('/views', express.static('views'));
+app.use('/static', express.static('static'));
+
 
 const conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
-  database: 'questions',
+  database: 'quiz',
 });
 
 app.get('/', (req, res) => {
@@ -23,6 +25,44 @@ app.get('/', (req, res) => {
 app.get('/', (req, res) => {
   res.json({
     message: 'HIIII',
+  });
+});
+//a question tableből kiválasztok egy random kérdést: random sorrendbe teszem az sql választ
+//This endpoint should return a random question with its answers.
+app.get('/game', (req, res) => {
+  let sql = 'SELECT * FROM questions ORDER BY RAND() LIMIT 1;';
+  conn.query(sql, (err, questions) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+      return;
+    }
+    let queryInput = questions[0].id;
+    conn.query('SELECT * FROM answers WHERE question_id = ?;', queryInput, (err, answers) => {
+      if (err) {
+        console.log(err);
+        res.sendStatus(500);
+        return;
+      }
+      res.json({
+        id: questions[0].id,
+        question: questions[0].question,
+        answers,
+      });
+    });
+  });
+});
+//This endpoint should return all the questions.
+app.get('/questions', (req, res) => {
+  conn.query('SELECT * FROM questions;', (err, questions) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+      return;
+    }
+    res.json({
+      questions,
+    });
   });
 });
 
