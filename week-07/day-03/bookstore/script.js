f45d397b351035e26358d3e3b1ac3903803eb5a3
Sorsi <orsolya.suto.so@gmail.com>
2018-07-31 15:43:31 +0200

    modified

--- week-07/day-03/bookstore/script.js
+++ week-07/day-03/bookstore/script.js
@@ -5,14 +5,14 @@ const http = new XMLHttpRequest();
 http.open('GET', 'http://localhost:3000/api/title', true); //ezzel lekértem az adatot a serverről, ami már lekérte az adatot az adatbázisból
 
 http.onload = () => {
- const data = JSON.parse(http.responseText);
- //console.log(data)
- 
- let list = document.querySelector('ul');
- 
- data.title.forEach((element) => {
-   const listElement = document.createElement('li');
-   list.appendChild(listElement).textContent = element.book_name;
+  const data = JSON.parse(http.responseText);
+  //console.log(data)
+
+  let list = document.querySelector('ul');
+
+  data.title.forEach((element) => {
+    const listElement = document.createElement('li');
+    list.appendChild(listElement).textContent = element.book_name;
   })
 }
 
